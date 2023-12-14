4dbb39ce809c5a318667cda87cf7603536d2947f
huy.nguyen <nguyenquochuy.cta@gmail.com>
2023-12-14 20:43:05 +0700

    Set up service provider

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import federation from '@originjs/vite-plugin-federation';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      // Modules to expose
+      exposes: {
+        './Button': './src/components/Button.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    minify: false,
+    cssCodeSplit: false,
+    target: 'esnext',
+  },
+});
